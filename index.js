2d9d237835cfcf9483bd57450edf6fbb607dd34d
Bamidele Aminu <obedaminu303@gmail.com>
2024-04-15 15:00:44 +0100

    checked current

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const http = require('http');
 const socketIo = require('socket.io');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 require('dotenv').config()
 const Message = require('./models/message')
 
@@ -13,7 +14,7 @@ const { chat_router } = require('./routes/chatRoutes')
 
 const port = process.env.App_Port || 4000;
 //deployment key
-// const mongo_url = process.env.Mongo_Url
+//const mongo_url = process.env.Mongo_Url
 //teting phase
 
 const mongo_url = 'mongodb://localhost:27017'
@@ -42,10 +43,16 @@ app.use(express.static('public'))
 // app.use('/uploads',express.static('uploads'))
 app.use(morgan('dev'))
 
+const store = new MongoDBStore({
+  uri: mongo_url,
+  collection: 'sessions'
+});
+
 app.use(session({
   secret: 'secret',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store
 }));
 
 const userSockets = {};
@@ -57,13 +64,13 @@ io.on('connection', (socket) => {
     console.log(`User ${userId} connected`);
     console.log(userSockets)
   });
-  
+
   // socket.on('sendMessage', async ({ senderId, receiverId, content }) => {
   //   try {
   //     // Save message to database
   //     const message = new Message({ sender: senderId, receiver: receiverId, content });
   //     await message.save();
-      
+
   //     // Emit the message to the receiver
   //     io.to(receiverId).emit('newMessage', { senderId, content });
   //   } catch (error) {
@@ -74,22 +81,22 @@ io.on('connection', (socket) => {
   socket.on('sendMessage', async (messageData) => {
     try {
       console.log('Message received:', messageData);
-      const { senderId, receiverId, message } = messageData;
+      const { senderId, receiverId, message, time } = messageData;
       const conversationId = generateConversationId(senderId, receiverId);
-      const newMessage = new Message({ senderId, receiverId, message, conversationId });
+      const newMessage = new Message({ senderId, receiverId, message, conversationId, time });
       await newMessage.save();
       console.log(newMessage)
       const recipients = [senderId, receiverId];
       // Emit the message only to the sockets of the recipients
-     
-        io.to(userSockets[receiverId]).emit('receiveMessage', newMessage);
-      
-     
-        io.to(userSockets[senderId]).emit('receiveMessage', newMessage);
-     
+
+      io.to(userSockets[receiverId]).emit('receiveMessage', newMessage);
+
+
+      io.to(userSockets[senderId]).emit('receiveMessage', newMessage);
+
 
       // Emit the message to the receiver's socket
-      
+
       // io.emit('receiveMessage', ); // Send message to sender and receiver
     } catch (err) {
       console.error(err);
@@ -121,7 +128,7 @@ app.get('/', (req, res) => {
 
 app.use('/blogs', router)
 app.use('/user', user_router)
-app.use('/chats',chat_router)
+app.use('/chats', chat_router)
 
 // app.use((req, res, next) => {
 //   if (req.session && !req.session.user) {
--- models/message.js
+++ models/message.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
-const date = new Date()
-const hour = date.getHours()
-const minute = date.getMinutes()
-const newMinute = minute < 10 ? "0" + minute : minute
-const md = hour < 12 ? "am" : "pm";
-const newHour = hour > 12 ? hour - 12 : hour
+
 
 const messageSchema = new Schema({
     senderId: { 
@@ -27,7 +22,7 @@ const messageSchema = new Schema({
     },
     time:{
         type: String, 
-        default: `${newHour}:${newMinute} ${md}`
+        required:true
     },
     timestamp: { 
         type: Date, 

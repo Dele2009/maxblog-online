c787b97f14582053a01b8f9d4ff00b08384a1b74
Bamidele Aminu <obedaminu303@gmail.com>
2024-05-20 11:48:19 +0100

    new update now-6

--- controllers/blog-controls.js
+++ controllers/blog-controls.js
@@ -20,23 +20,34 @@ const { formatDistanceToNow } = require('date-fns')
 const get_blogs = async (req, res) => {
   try {
     // const result = await Blog.find()
-
+    const page = parseInt(req.query.page) || 1; // Current page number, default to 1 if not provided
+    const perPage = 6;
     let user;
     if (req.session && req.session.user) {
       user = req.session.user
     }
-    // const totalCount = await Newblogs.countDocuments();
+    const totalCount = await Newblogs.countDocuments();
 
     // // Calculate total number of pages
-    // const totalPages = Math.ceil(totalCount / perPage);
+    const totalPages = Math.ceil(totalCount / perPage);
+    const skip = Number((page - 1) * perPage)
+    const limit = Number(perPage)
     const result = await Newblogs.find()
-    const sliderBlogs = result.slice(0, 4).sort((a, b) => a.createdAt - b.createdAt)
-    const allresults = result.slice(0, 6).sort((a, b) => a.createdAt - b.createdAt)
+    const sliderBlogs = result.slice(0, 4).sort((a, b) => b.createdAt - a.createdAt)
+    const allresults = result.sort((a, b) => a.createdAt - b.createdAt)
+                              .slice(skip, skip + limit)
+
     const newresults = result.slice().sort((a, b) => b.createdAt - a.createdAt)
 
     res.render('index', {
       title: 'Home',
+
       Blogs: allresults,
+      totalCount,
+      currentPage: page,
+      totalPages,
+      contentPerPage: perPage,
+
       newBlogs: newresults,
       slideBlogs: sliderBlogs,
       formatDistanceToNow,
@@ -64,12 +75,12 @@ const get_a_blog = async (req, res) => {
     const requestedBlog = await Newblogs.findById(id)
     const result = await Newblogs.find()
     const newresults = result.slice().sort((a, b) => b.createdAt - a.createdAt)
-    res.render('blogView', 
-      { 
+    res.render('blogView',
+      {
         title: 'blog Details',
-        Blog: requestedBlog, 
+        Blog: requestedBlog,
         newBlogs: newresults,
-        user 
+        user
       }
     )
   } catch (error) {
@@ -95,14 +106,15 @@ const get_blog_category = async (req, res) => {
     // Calculate total number of pages
     const totalPages = Math.ceil(totalCount / perPage);
     const result = await Newblogs.find({ category: id }).sort({ createdAt: -1 })
-    .skip((page - 1) * perPage)
-    .limit(perPage);
+      .skip((page - 1) * perPage)
+      .limit(perPage);
     res.render('blogcategory',
       {
         title: `${id}`,
         Blogs: result,
         user,
         formatDistanceToNow,
+        totalCount,
         currentPage: page,
         totalPages: totalPages,
         contentPerPage: perPage,
